c36786e1dc9572515ec5021607f07e197e2e0cd9
Александр Бабаев <babaev@atoms.ru>
2022-12-02 17:25:41 +0300

    speech UI

---
+++ src/app.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import './app.css';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+const App = () => {
+  const { transcript, finalTranscript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  useEffect(() => {
+    if (browserSupportsSpeechRecognition) {
+      SpeechRecognition.startListening({ continuous: true, language: 'ru-RU' }).then(() => {})
+    } else {
+      alert('Упс, ваш браузер не поддерживает распознавание речи!');
+    }
+  }, [])
+
+  useEffect(() => {
+    if (finalTranscript) resetTranscript()
+  }, [finalTranscript]);
+
+  return (
+    <div className="app">
+      <div>
+        {transcript ? 'Фраза, которую вы сейчас назвали' : 'Назовите свое слово или фразу'}
+        <br />
+        <br />
+        {transcript ? (
+            <div style={{ textAlign: 'center', fontSize: '170%', fontWeight: 700 }}>{transcript}</div>
+          ) : (
+            <div style={{ textAlign: 'center' }}>...</div>
+          )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
